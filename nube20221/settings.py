1e9deae3a64816af492edfa4a047300d28a73465
Juanca101010 <juancristanchom@usantotomas.edu.co>
2022-05-09 15:54:42 -0500

    n

--- nube20221/settings.py
+++ nube20221/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'nube20221.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'nube20221uno',
+        'USER': 'postgres',
+        'PASSWORD': '12345678',
+        'HOST': 'localhost',
+        'DATEBASE_PORT': '5432',
     }
 }
 
